23a042efef022dbafb8eaf79445c4ca6b3487374
victorjurj <victor.jurj@yahoo.com>
2023-09-25 20:43:50 +0300

    implement Hibernate work Session

---
+++ src/main/java/com/practice/util/HibernateUtil.java
@@ -0,0 +1,13 @@
+package com.practice.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    public static Session getSession() {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        return factory.openSession();
+    }
+}
